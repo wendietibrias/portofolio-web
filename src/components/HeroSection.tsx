35a95eac8161d2cabd5d817719754b625cb7e69a
wendietibrias <wendietibrias@gmail.com>
2023-08-24 21:40:49 +0700

    fix hero section layout

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -20,11 +20,11 @@ const HeroSection = () => {
     return (
         <motion.div initial="hidden" whileInView="visible" variants={variant} className="text-center sm:px-5 text-white w-full h-full relative flex flex-col justify-center items-center"> 
             <h4 className="text-2xl sm:text-xl font-normal">Welcome,</h4>
-            <h2 className="text-5xl sm:text-2xl font-medium mt-2 mb-4">
+            <h2 className="text-5xl sm:text-2xl font-medium mt-2 mb-5">
                 I'am A <span  className="text-blue-500 font-semibold">Fullstack Developer</span>
             </h2>
-            <p className="text-[13px] sm:text-[12px] text-gray-300 mx-auto text-center w-[450px] sm:px-8 sm:w-full"> 
-              I am someone who has an interest in developing a website using modern frameworks that are currently trending like Next.js.
+            <p className="text-[13px] sm:text-[12px] text-gray-300 mx-auto text-center w-[400px] sm:px-8 xs:px-2 sm:w-full"> 
+              I am someone who has an interest in developing a website using modern javascript frameworks.
             </p>
             <Link href="resume/Wendi-CV.pdf" download="wendi-cv" className="mt-9">
                 <button className="text-white font-semibold text-sm bg-blue-500  transition-colors duration-200 rounded-full py-2 px-6 border-2 border-blue-500  hover:text-blue-500 hover:bg-transparent">Download CV</button>
